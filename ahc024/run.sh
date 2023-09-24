6ce79da04a48ca6195d3aeebe8e6e3c8d916908a
Yoke <06106186@gmail.com>
2023-09-24 20:47:14 +0900

    ahc024

---
+++ ahc024/run.sh
@@ -0,0 +1,3 @@
+
+cargo run --features local < in > out
+# cargo run --features local --features seed < in > out
---
+++ ahc024/src/main.rs
@@ -0,0 +1,195 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::needless_range_loop)]
+#![allow(dead_code)]
+
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque},
+};
+
+use itertools::Itertools;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+use superslice::Ext;
+
+mod rnd {
+    static mut S: usize = 0;
+    static MAX: usize = 1e9 as usize;
+
+    #[inline]
+    pub fn init(seed: usize) {
+        unsafe {
+            if seed == 0 {
+                let t = std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs() as usize;
+                S = t
+            } else {
+                S = seed;
+            }
+        }
+    }
+    #[inline]
+    pub fn gen() -> usize {
+        unsafe {
+            if S == 0 {
+                init(0);
+            }
+            S ^= S << 7;
+            S ^= S >> 9;
+            S
+        }
+    }
+    #[inline]
+    pub fn gen_range(a: usize, b: usize) -> usize {
+        gen() % (b - a) + a
+    }
+    #[inline]
+    pub fn gen_bool() -> bool {
+        gen() & 1 == 1
+    }
+    #[inline]
+    pub fn gen_range_isize(a: usize) -> isize {
+        let mut x = (gen() % a) as isize;
+        if gen_bool() {
+            x *= -1;
+        }
+        x
+    }
+    #[inline]
+    pub fn gen_range_neg_wrapping(a: usize) -> usize {
+        let mut x = gen() % a;
+        if gen_bool() {
+            x = x.wrapping_neg();
+        }
+        x
+    }
+    #[inline]
+    pub fn gen_float() -> f64 {
+        ((gen() % MAX) as f64) / MAX as f64
+    }
+}
+
+#[derive(Debug, Clone)]
+struct TimeKeeper {
+    start_time: std::time::Instant,
+    time_threshold: f64,
+}
+
+impl TimeKeeper {
+    fn new(time_threshold: f64) -> Self {
+        TimeKeeper {
+            start_time: std::time::Instant::now(),
+            time_threshold,
+        }
+    }
+    #[inline]
+    fn isTimeOver(&self) -> bool {
+        let elapsed_time = self.start_time.elapsed().as_nanos() as f64 * 1e-9;
+        #[cfg(feature = "local")]
+        {
+            elapsed_time * 0.55 >= self.time_threshold
+        }
+        #[cfg(not(feature = "local"))]
+        {
+            elapsed_time >= self.time_threshold
+        }
+    }
+    #[inline]
+    fn get_time(&self) -> f64 {
+        let elapsed_time = self.start_time.elapsed().as_nanos() as f64 * 1e-9;
+        #[cfg(feature = "local")]
+        {
+            elapsed_time * 0.55
+        }
+        #[cfg(not(feature = "local"))]
+        {
+            elapsed_time
+        }
+    }
+}
+
+const N: usize = 50;
+const M: usize = 100;
+const INF: usize = 1_usize << 60;
+const DIJ4: [(usize, usize); 4] = [(!0, 0), (0, !0), (1, 0), (0, 1)];
+
+#[derive(Debug, Clone)]
+struct State {
+    C: Vec<Vec<usize>>,
+}
+
+impl State {
+    fn read() -> Self {
+        input! {
+            _N: usize,
+            _M: usize,
+            C: [[usize; N]; N]
+        }
+
+        State { C }
+    }
+    fn output(&self) {
+        for row in &self.C {
+            println!("{}", row.iter().join(" "));
+        }
+    }
+}
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    fn solve(&mut self) {
+        let start = std::time::Instant::now();
+
+        #[cfg(feature = "local")]
+        {
+            eprintln!("Local Mode");
+            rnd::init(1);
+        }
+        let state = State::read();
+        state.output();
+
+        #[allow(unused_mut, unused_assignments)]
+        let mut elapsed_time = start.elapsed().as_micros() as f64 * 1e-6;
+        #[cfg(feature = "local")]
+        {
+            eprintln!("Local Mode");
+            elapsed_time *= 0.55;
+        }
+        eprintln!("Elapsed: {}", (elapsed_time * 1000.0) as usize);
+    }
+}
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
