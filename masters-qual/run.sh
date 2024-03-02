f405fd9a973617e3266b5628ecd7b17c2321be6c
Yoke <06106186@gmail.com>
2024-03-02 14:51:33 +0900

    masters qual

---
+++ masters-qual/run.sh
@@ -0,0 +1,3 @@
+
+#cargo run --bin a --features local < tools/in/0000.txt > out
+cargo run -r --manifest-path tools/Cargo.toml --bin tester cargo run -r --bin a < in > out
---
+++ masters-qual/src/bin/a.rs
@@ -0,0 +1,147 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::needless_range_loop)]
+#![allow(dead_code)]
+
+use std::{
+    cmp::Reverse,
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashSet, VecDeque},
+};
+
+use itertools::Itertools;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
+use rand::prelude::*;
+use superslice::Ext;
+
+fn main() {
+    let start = std::time::Instant::now();
+
+    let mut stdin =
+        proconio::source::line::LineSource::new(std::io::BufReader::new(std::io::stdin()));
+    macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
+
+    input! {}
+
+    #[allow(unused_mut, unused_assignments)]
+    let mut elapsed_time = start.elapsed().as_micros() as f64 * 1e-6;
+    #[cfg(feature = "local")]
+    {
+        eprintln!("Local Mode");
+        elapsed_time *= 0.55;
+    }
+    eprintln!("Elapsed: {}", (elapsed_time * 1000.0) as usize);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Coord {
+    row: usize,
+    col: usize,
+}
+
+impl Coord {
+    pub fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+    pub fn in_map(&self, height: usize, width: usize) -> bool {
+        self.row < height && self.col < width
+    }
+    pub fn to_index(&self, width: usize) -> CoordIndex {
+        CoordIndex(self.row * width + self.col)
+    }
+}
+
+impl std::ops::Add<CoordDiff> for Coord {
+    type Output = Coord;
+    fn add(self, rhs: CoordDiff) -> Self::Output {
+        Coord::new(
+            self.row.wrapping_add_signed(rhs.dr),
+            self.col.wrapping_add_signed(rhs.dc),
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct CoordDiff {
+    dr: isize,
+    dc: isize,
+}
+
+impl CoordDiff {
+    pub const fn new(dr: isize, dc: isize) -> Self {
+        Self { dr, dc }
+    }
+}
+
+pub const ADJ: [CoordDiff; 4] = [
+    CoordDiff::new(1, 0),
+    CoordDiff::new(!0, 0),
+    CoordDiff::new(0, 1),
+    CoordDiff::new(0, !0),
+];
+
+pub struct CoordIndex(pub usize);
+
+impl CoordIndex {
+    pub fn new(index: usize) -> Self {
+        Self(index)
+    }
+    pub fn to_coord(&self, width: usize) -> Coord {
+        Coord {
+            row: self.0 / width,
+            col: self.0 % width,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DynamicMap2d<T> {
+    pub size: usize,
+    map: Vec<T>,
+}
+
+impl<T> DynamicMap2d<T> {
+    pub fn new(map: Vec<T>, size: usize) -> Self {
+        assert_eq!(size * size, map.len());
+        Self { size, map }
+    }
+}
+
+impl<T> std::ops::Index<Coord> for DynamicMap2d<T> {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, coordinate: Coord) -> &Self::Output {
+        &self[coordinate.to_index(self.size)]
+    }
+}
+
+impl<T> std::ops::IndexMut<Coord> for DynamicMap2d<T> {
+    #[inline]
+    fn index_mut(&mut self, coordinate: Coord) -> &mut Self::Output {
+        let size = self.size;
+        &mut self[coordinate.to_index(size)]
+    }
+}
+
+impl<T> std::ops::Index<CoordIndex> for DynamicMap2d<T> {
+    type Output = T;
+
+    fn index(&self, index: CoordIndex) -> &Self::Output {
+        unsafe { self.map.get_unchecked(index.0) }
+    }
+}
+
+impl<T> std::ops::IndexMut<CoordIndex> for DynamicMap2d<T> {
+    #[inline]
+    fn index_mut(&mut self, index: CoordIndex) -> &mut Self::Output {
+        unsafe { self.map.get_unchecked_mut(index.0) }
+    }
+}
